7075805e8a2d7826fe0dd06943b75c497d67a1b0
AnishaZ <anishagzacharia@gmail.com>
2017-05-23 19:59:14 -0700

    Updated friends-show component to be able to view each friend's page

--- public/src/app/friends/friends-show/friends-show.component.ts
+++ public/src/app/friends/friends-show/friends-show.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { FriendsService } from './../friends.service';
 @Component({
   selector: 'app-friends-show',
   templateUrl: './friends-show.component.html',
   styleUrls: ['./friends-show.component.css']
 })
 export class FriendsShowComponent implements OnInit {
-
-  constructor() { }
+	friend_id: String;
+	friend = {};
+  constructor(
+  	private _route: ActivatedRoute,
+  	private _friendsService: FriendsService) { }
 
   ngOnInit() {
+  	 this._route.params.subscribe((param)=>{
+      this.friend_id = param.id;
+    })
+
+  	 this.getFriend(this.friend_id);
+  }
+
+  getFriend(id){
+  	this._friendsService.getFriend(id)
+  		.then( friend => this.friend = friend)
+  		.catch( err => console.log(err))
   }
 
 }
