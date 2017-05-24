5683190e8e57e1075cc87013993ad3c5aaf92eda
AnishaZ <anishagzacharia@gmail.com>
2017-05-23 18:18:16 -0700

    Added back end routing and controller methods for getting all friends, updating a friend, adding a new friend, and showing a friend's page

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,8 @@
 let controller = require('./../controllers/controller');
 
 module.exports = app => {
-  app.get('/api/friends', controller.getAll());
+  app.get('/api/friends', controller.getAll);
+  app.post('/api/friends', controller.addFriend);
+  app.get('/api/friends/:id', controller.getFriend);
+  app.put('/api/friends/:id', controller.updateFriend);
 }
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -12,6 +12,40 @@ module.exports = {
         return res.json(friends);
       }
     })
-  }
+  },
+
+  addFriend: (req,res) => {
+    var newFriend = new Friend(req.body);
+    newFriend.save( (err, friend) => {
+			if(err){
+				console.log(err);
+				return res.sendStatus(500);
+			}else{
+				return res.json(friend);
+			}
+		})
+  },
+
+  updateFriend: (req,res) => {
+    Friend.update({_id: req.body._id}, req.body, (err, data)=> {
+			if(err){
+				console.log(err);
+				return res.sendStatus(500)
+			}else{
+				return res.json(data);
+			}
+		})
+	},
+
+  getFriend: (req,res) => {
+    Friend.findOne({_id: req.params.id}, (err, friend) => {
+			if(err){
+				console.log(err);
+				return res.sendStatus(500);
+			}else{
+				return res.json(friend);
+			}
+		})
+	}
 
 }
