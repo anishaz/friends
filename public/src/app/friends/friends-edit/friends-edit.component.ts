b538bcb2bd593798e0a69c9d410171e38bbd7de2
AnishaZ <anishagzacharia@gmail.com>
2017-05-23 19:55:54 -0700

    Updated friends-edit component html and ts

--- public/src/app/friends/friends-edit/friends-edit.component.ts
+++ public/src/app/friends/friends-edit/friends-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FriendsService } from './../friends.service';
 
 @Component({
   selector: 'app-friends-edit',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friends-edit.component.css']
 })
 export class FriendsEditComponent implements OnInit {
-
-  constructor() { }
+	friend_id: String;
+	friend;
+  constructor(private _route: ActivatedRoute,
+  	private _friendsService: FriendsService,
+  	private router: Router) { }
 
   ngOnInit() {
+  	this._route.params.subscribe((param)=>{
+      this.friend_id = param.id;
+    })
+
+  	this.getFriend(this.friend_id);
+  }
+
+  getFriend(id){
+  	this._friendsService.getFriend(id)
+  		.then( friend => this.friend = friend)
+  		.catch( err => console.log(err))
+  }
+
+  editFriend(){
+  	this._friendsService.editFriend(this.friend)
+  		.then(() => this.router.navigate(['/friends']))
+  		.catch( err => console.log(err))
   }
 
 }
--- public/src/app/friends/friends.component.ts
+++ public/src/app/friends/friends.component.ts
@@ -7,10 +7,19 @@ import { FriendsService } from './friends.service';
   styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent implements OnInit {
-
+  friends: Array<any>;
   constructor(private _FriendsService: FriendsService) { }
 
   ngOnInit() {
+    this.getAllFriends();
+  }
+
+  getAllFriends(){
+  	this._FriendsService.getFriends()
+  		.then( friends => {
+  			console.log(friends);
+  			this.friends = friends})
+  		.catch( response => console.log(response) )
   }
 
 }
